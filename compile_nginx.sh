d0e22686b533b035223e99eb8cd8f8c711127b14
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-12-23 22:53:50 +0100

    Build Nginx  modules on the fly

---
+++ compile_nginx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+#set versions
+nginx_version="1.21.4"
+zlib_version="1.2.11"
+
+
+
+wget "https://nginx.org/download/nginx-$version.tar.gz" | tar -xf
+
+# nginx_cache_purge
+git clone https://github.com/nginx-modules/ngx_cache_purge
+
+# brotili 
+git clone https://github.com/google/ngx_brotli.git
+
+# Download zlib
+wget "https://zlib.net/zlib-$zlib_version.tar.gz" | tar -xf
+
+cd ./nginx-$nginx_version/
+
+./configure --with-compat --add-dynamic-module=../ngx_brotli --with-zlib=../zlib-1.2.11 --add-dynamic-module=../ngx_cache_purge
+make modules 
+tar -xcvf modules.tar.gz ./objs/*.so
---
+++ docs/ngx_http_cache_purge.md
@@ -0,0 +1,7 @@
+# ngx_http_purge.md 
+
+Originally made by @FRiCKLE (https://github.com/FRiCKLE/ngx_cache_purge) 
+
+# Usage 
+
+See attached template
