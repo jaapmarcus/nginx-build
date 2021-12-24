d5a3d09826cfa97a8ff39f6930a34fd2f61827bd
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-12-24 11:44:23 +0100

    Fix bug in tar command

--- compile_nginx.sh
+++ compile_nginx.sh
@@ -5,8 +5,8 @@ nginx_version="1.21.4"
 zlib_version="1.2.11"
 
 
-
-wget "https://nginx.org/download/nginx-$nginx_version.tar.gz" | tar -xf
+cd ./
+wget "https://nginx.org/download/nginx-$nginx_version.tar.gz" | tar -xz
 
 # nginx_cache_purge
 git clone https://github.com/nginx-modules/ngx_cache_purge
@@ -15,7 +15,7 @@ git clone https://github.com/nginx-modules/ngx_cache_purge
 git clone https://github.com/google/ngx_brotli.git
 
 # Download zlib
-wget "https://zlib.net/zlib-$zlib_version.tar.gz" | tar -xf
+wget "https://zlib.net/zlib-$zlib_version.tar.gz" | tar -xz
 
 cd ./nginx-$nginx_version/
 
